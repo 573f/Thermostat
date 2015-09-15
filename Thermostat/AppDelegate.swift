ba0c08ef0bed30da82441b051bc5143736c89ac6
Stephen Skubik-Peplaski <codesmith@me.com>
2015-09-15 14:34:46 -0400

    Initial Commit

---
+++ Thermostat/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Thermostat
+//
+//  Created by Stephen Skubik-Peplaski on 9/15/15.
+//  Copyright © 2015 Stephen Skubik-Peplaski. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
